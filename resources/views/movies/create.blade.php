15c3a7440a800cfa7d174ebabb29955daebab45e
Aljirana Mawaddah <aljiranamawaddah1612-dev.com>
2026-04-23 20:14:25 +0700

    feat: penyiapan blade component untuk layout utama

---
+++ resources/views/movies/create.blade.php
@@ -0,0 +1,8 @@
+<x-app>
+
+
+    <x-slot:title> {{ $title }}</x-slot>
+
+    <h1 class="fw-bold">Form create Movies</h1>
+
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('movies', function () {
+    return view('movies.index', ['title' => 'Movies']);
+});
+
+Route::get('movies/create', function () {
+    return view('movies.create', ['title' => 'Create Movies']);
+});
